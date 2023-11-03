c89c6816c82044f129ac4eaef992d63a6c26ed6d
manggury <148739312+manggury@users.noreply.github.com>
2023-11-03 17:39:15 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,22 @@
 $(function () {
     $('.Visualslide').slick({
         arrows: false,
+        autoplay: true,
+        dots: true,
     })
+
+    $('.totop').on('click', function () {
+        $('html, body').animate({ scrollTop: 0 }, 800)
+    });
+
+    $(window).on('scroll', function () {
+        const sct = $(window).scrollTop();
+        if (sct > 300) {
+            $('.totop').addClass('on');
+            $('.Header').addClass('on');
+        } else {
+            $('.totop').removeClass('on');
+            $('.Header').removeClass('on');
+        }
+    });
 });
